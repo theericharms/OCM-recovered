0e6a9b122bd69f6128b02cb13c2d6439d5744d13
eric harms <eric.harms@gmail.com>
2020-03-18 15:37:54 -0700

    starting up again

---
+++ OCMAdmin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OCMAdmin.Models;
+
+[assembly: HostingStartup(typeof(OCMAdmin.Areas.Identity.IdentityHostingStartup))]
+namespace OCMAdmin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OCMAdminContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OCMAdminContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<OCMAdminContext>();
+            });
+        }
+    }
+}
